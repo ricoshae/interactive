e0d3c0c8cda2aa1d83f31d97f46d7e141005457f
Christopher M. Richter <christophermrichter@users.noreply.github.com>
2026-02-10 08:00:58 +1000

    Initial

--- responsivecarousel-2025/script.js
+++ responsivecarousel-2025/script.js
@@ -1,59 +1,60 @@
-window.onload = function() {
- 
-var slideIndex = 1;
-(function () {
-   
-showSlides(slideIndex);
+window.onload = function () {
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+  var slideIndex = 1;
+  (function () {
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+    showSlides(slideIndex);
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
 
-var dots = document.querySelectorAll('.dot');
-var prev = document.querySelectorAll('.prev');
-var next = document.querySelectorAll('.next');
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
 
-[...prev].forEach((p)=>{
-  p.addEventListener( 'click', function() {
-plusSlides(-1);
-  });
-});
+    function showSlides(n) {
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("dot");
+      if (n > slides.length) { slideIndex = 1 }
+      if (n < 1) { slideIndex = slides.length }
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex - 1].style.display = "block";
+      dots[slideIndex - 1].className += " active";
+    }
 
-[...next].forEach((p)=>{
-  p.addEventListener( 'click', function() {
-plusSlides(1);
-  });
-});
+    var dots = document.querySelectorAll('.dot');
+    var prev = document.querySelectorAll('.prev');
+    var next = document.querySelectorAll('.next');
 
-dots.forEach((item, index) => { 
+    [...prev].forEach((p) => {
+      p.addEventListener('click', function () {
+        plusSlides(-1);
+      });
+    });
+
+    [...next].forEach((p) => {
+      p.addEventListener('click', function () {
+        plusSlides(1);
+      });
+    });
+
+    dots.forEach((item, index) => {
       item.addEventListener('click', arrow => {
-      currentSlide(index);
-})})
+        currentSlide(index);
+      })
+    })
 
 
 
-})();
+  })();
 
 
 
-   };
+};
